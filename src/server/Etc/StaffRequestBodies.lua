e8513a398eb8dd34619ba1a4de8b32635d4d5ed2
mattyxcx <mattwyc.roblox@gmail.com>
2021-11-09 19:32:02 +0000

    call aesthetic update

--- src/server/Etc/StaffRequestBodies.lua
+++ src/server/Etc/StaffRequestBodies.lua
@@ -10,10 +10,10 @@ function Module.CreateBody(Caller,Note)
                  ["name"] = Caller.Name
               },
               ["color"] = 2097286,
-              ["description"] = [[> **]] .. Caller.Name .. [[** needs extra assistance in **]] .. ServerInfo.LoopName.Value .. [[**! Please enter this loop and offer your assistance if you happen to be available...
+              ["description"] = [[> ]] .. Caller.Name .. [[ needs extra assistance in ]] .. ServerInfo.LoopName.Value .. [[! Please enter this loop and offer your assistance if you happen to be available...
      
      - ***faulty phone line connects*** -
-     > *"]] .. Note .. [["* - **]] .. Caller.Name .. [[**
+     > *"]] .. Note .. [[" - ]] .. Caller.Name .. [[*
      - ***line is severed*** -
      
      > [*Enter loop*](https://www.roblox.com/games/6664565660/The-Hidden-Academy?gameId=]] .. ServerInfo.JobId.Value .. [[)]],
@@ -26,17 +26,17 @@ function Module.CreateBody(Caller,Note)
                  [2] =  {
                     ["inline"] = true,
                     ["name"] = "Peculiar Headcount",
-                    ["value"] = "```" .. #game.Players:GetPlayers() .. "```"
+                    ["value"] = "```" .. #game.Players:GetPlayers()-ServerInfo.Staff.Value .. "```"
                  },
                  [3] =  {
                     ["inline"] = true,
                     ["name"] = "Loop Time",
                     ["value"] = "```" .. game.Lighting.TimeOfDay .. "```"
                  }
-              },
-              ["title"] = Caller.Name .. " is calling for your help!"
+              }
            }
         }
+    }
 end
 
 return Module
