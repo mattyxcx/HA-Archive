db52386c86c75552ebec662a6be65d61daf9d24f
mattyxcx <mattwyc.roblox@gmail.com>
2021-11-21 16:56:14 +0000

    made code readable & added multi-use functions

--- src/server/Etc/DataHandler/init.lua
+++ src/server/Etc/DataHandler/init.lua
@@ -1,60 +1,38 @@
-local module = {}
-local proxyurl = "https://thehiddenacademy.herokuapp.com/"
-local baseurl,parenturl
-local https = game:GetService("HttpService")
-local http = require(script.ProxyService) 
-local translatetypes =  {
-	["string"]="stringValue",
-	["boolean"]="booleanValue",
-	["number"]="doubleValue",
-	["table"] = "mapValue"
-}
-local translateop= {
-	["<"] = "LESS_THAN",
-	["<="] = "LESS_THAN_OR_EQUAL",
-	[">"] = "GREATER_THAN",
-	[">="] = "GREATER_THAN_OR_EQUAL",
-	["="] = "EQUAL",
-	["~="] = "NOT_EQUAL"
-}
+local DataHandler = {}
+local HttpService = game:GetService("HttpService")
+local ProxyService = require(script.ProxyService) 
+local ProxyUrl = "https://thehiddenacademy.herokuapp.com/"
 
-function module.Setup(accesskey,id)
-	baseurl = "https://firestore.googleapis.com/v1beta1/projects/"..id.."/databases/(default)/documents"
-	parenturl = "https://firestore.googleapis.com/v1beta1/projects/"..id.."/databases/(default)"
-	http:New(proxyurl,accesskey)
+function DataHandler.Setup(AccessKey)
+	ProxyService:New(ProxyUrl,AccessKey)
 end
 
-function module.Get(datastore,key)
-	local response = nil
-	local url = baseurl.."/"..datastore.."/"..key
-	local success, err = pcall(function()
-		response = http:Get(url)
+function DataHandler.Get(URL)
+	local Response = nil
+	local Success,Error = pcall(function()
+		Response = ProxyService:Get(URL)
 	end)
-	if success then
-		local data = https:JSONDecode(response.body)
-		return data
+	if Success then
+		return HttpService:JSONDecode(Response.body)
 	end
-	if err then
+	if Error then
 		return "Error"
 	end
 end
 
-function module.Set(Datastore,key,data)
-	local response = nil
-	local body = https:JSONEncode(data)
-	local url = baseurl.."/"..Datastore.."/"..key
-	local success, err = pcall(function()
-		response = http:Patch(url,body)
+function DataHandler.Patch(URL,Data)
+	local Response = nil
+	local Body = HttpService:JSONEncode(Data)
+	local Success,Error = pcall(function()
+		Response = ProxyService:Patch(URL,Body)
 	end)
-	if success then
-		local rt = https:JSONDecode(response.body)
-		return rt
+	if Success then
+		return HttpService:JSONDecode(Response.body)
 	end
-	if err then
-		warn("Error at patchdocuments!")
+	if Error then
 		return "Error"
 	end
 end
 
-return module
+return DataHandler
 
